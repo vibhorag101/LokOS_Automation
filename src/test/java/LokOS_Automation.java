4d0bb3ddaf2d204b34d999200560523ffdfef69f
VIBHOR AGARWAL <vibhor20349@iiitd.ac.in>
2023-03-05 15:01:12 +0530

    Done First Page including File

--- src/test/java/LokOS_Automation.java
+++ src/test/java/LokOS_Automation.java
@@ -1,3 +1,5 @@
+import java.io.File;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -15,7 +17,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 public class LokOS_Automation {
     static AndroidDriver<MobileElement> driver;
     static WebDriverWait wait;
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException, IOException {
         openLokOS();
         navigateSHG();
         fillInfo();
@@ -25,51 +27,73 @@ public class LokOS_Automation {
                 + "new UiSelector().scrollable(true)).scrollIntoView(" + "new UiSelector().text(\"" + text + "\"));");
     }
 
-    public static MobileElement scrollToId(AndroidDriver<MobileElement> driver, String id) {
+    public static MobileElement scrollToId(String id) {
 
         return (MobileElement) driver.findElementByAndroidUIAutomator(
                 "new UiScrollable(" + "new UiSelector().scrollable(true)).scrollIntoView("
                         + "new UiSelector().resourceIdMatches(\"" + id + "\"));");
     }
 
-    public static void fillInfo(){
-        WebElement SHGName = wait.until(ExpectedConditions.presenceOfElementLocated(By.id("com.microware.cdfi.training:id/et_groupname")));
+    public static MobileElement selectDropdownText(String text){
+        WebElement dropdown = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//android.widget.TextView[@text='"+text+"']")));
+        return (MobileElement) dropdown;
+    }
+
+    public static void fillInfo() throws IOException {
+        // SHG Name
+        MobileElement SHGName = scrollToId("com.microware.cdfi.training:id/et_groupname");
         SHGName.sendKeys("Test SHG");
-        WebElement date = wait.until(ExpectedConditions.presenceOfElementLocated(By.id("com.microware.cdfi.training:id/et_formationDate")));
-        date.click();
 
+        // SHG Date
+        scrollToId("com.microware.cdfi.training:id/et_formationDate").click();
         wait.until(ExpectedConditions.presenceOfElementLocated(By.id("android:id/button1"))).click();
-        System.out.println("Date Selected");
 
-        WebElement promotedBy = wait.until(ExpectedConditions.presenceOfElementLocated(By.id("com.microware.cdfi.training:id/spin_promotedby")));
-        promotedBy.click();
+        // Promoted By
+        scrollToId("com.microware.cdfi.training:id/spin_promotedby").click();
+        selectDropdownText("NRLM").click();
 
-        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//android.widget.TextView[@text='NRLM']"))).click();
         // Meeting Frequency
-        wait.until(ExpectedConditions.presenceOfElementLocated(By.id("com.microware.cdfi.training:id/spin_frequency"))).click();
-        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//android.widget.TextView[@text='Weekly']"))).click();
+        scrollToId("com.microware.cdfi.training:id/spin_frequency").click();
+        selectDropdownText("Weekly").click();
+
+        // Meeting Day
+        scrollToId("com.microware.cdfi.training:id/spin_weekday").click();
+        selectDropdownText("Monday").click();
 
         // saving frequency
-        wait.until(ExpectedConditions.presenceOfElementLocated(By.id("com.microware.cdfi.training:id/spin_savingfrequency"))).click();
-        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//android.widget.TextView[@text='Weekly']"))).click();
+        scrollToId("com.microware.cdfi.training:id/spin_savingfrequency").click();
+        selectDropdownText("Weekly").click();
 
         // saving value
-        wait.until(ExpectedConditions.presenceOfElementLocated(By.id("com.microware.cdfi.training:id/et_saving"))).sendKeys("10");
+        scrollToId("com.microware.cdfi.training:id/et_saving").sendKeys("10");
 
         // primary activity
-        wait.until(ExpectedConditions.presenceOfElementLocated(By.id("com.microware.cdfi.training:id/spin_primaryActivity"))).click();
-        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//android.widget.TextView[@text='Agriculture Activities']"))).click();
+        scrollToId("com.microware.cdfi.training:id/spin_primaryActivity").click();
+        selectDropdownText("Agriculture Activities").click();
 
-        // primary activity
-        wait.until(ExpectedConditions.presenceOfElementLocated(By.id("com.microware.cdfi.training:id/spin_secondaryActivity"))).click();
-        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//android.widget.TextView[@text='Fishery Activities']"))).click();
+        // secondary activity
+        scrollToId("com.microware.cdfi.training:id/spin_secondaryActivity").click();
+        selectDropdownText("Fishery Activities").click();
 
         // tertiary activity
-        wait.until(ExpectedConditions.presenceOfElementLocated(By.id("com.microware.cdfi.training:id/spin_tertiaryActivity"))).click();
-        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//android.widget.TextView[@text='Horticulture Activities']"))).click();
+        scrollToId("com.microware.cdfi.training:id/spin_tertiaryActivity").click();
+        selectDropdownText("Horticulture Activities").click();
 
         // tenure of office bearers
-        wait.until(ExpectedConditions.presenceOfElementLocated(By.id("com.microware.cdfi.training:id/et_electiontenure"))).sendKeys("12");
+        scrollToId("com.microware.cdfi.training:id/et_electiontenure").sendKeys("12");
+
+        // Resolution File
+        scrollToId("com.microware.cdfi.training:id/Imgmember").click();
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.id("com.microware.cdfi.training:id/tvUploadData"))).click();
+
+        // push a file to the device using appium
+        String filePath = "src/test/java/SHGCopy.pdf";
+        driver.pushFile("/storage/emulated/0/Download/SHGCopy.pdf", new File(filePath));
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//android.widget.TextView[contains(@text,'SHGCopy')]"))).click();
+
+
+
+
 
 
 
@@ -93,7 +117,7 @@ public class LokOS_Automation {
         capabilities.setCapability("appPackage", "com.microware.cdfi.training");
         capabilities.setCapability("appActivity", "com.microware.cdfi.activity.SplashScreenActivity");
         capabilities.setCapability("noReset", "true");
-        capabilities.setCapability("newCommandTimeout", 20);
+        capabilities.setCapability("newCommandTimeout", 200);
 
 
         URL url;
