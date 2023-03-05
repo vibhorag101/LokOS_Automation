f21d1aa5f2defe34fcd442cac1f83693ee4415e0
VIBHOR AGARWAL <vibhor20349@iiitd.ac.in>
2023-03-05 20:13:45 +0530

    Member Creation Logic First Page Done

--- src/test/java/LokOS_Automation.java
+++ src/test/java/LokOS_Automation.java
@@ -21,12 +21,6 @@ public class LokOS_Automation {
     static AndroidDriver<MobileElement> driver;
     static WebDriverWait wait;
 
-    public static void main(String[] args) throws InterruptedException, IOException {
-        openLokOS();
-        navigateSHG();
-        fillInfo();
-    }
-
     public static MobileElement scrollToText(AndroidDriver<MobileElement> driver, String text) {
         return (MobileElement) driver.findElementByAndroidUIAutomator("new UiScrollable("
                 + "new UiSelector().scrollable(true)).scrollIntoView(" + "new UiSelector().text(\"" + text + "\"));");
@@ -43,6 +37,10 @@ public class LokOS_Automation {
         WebElement dropdown = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//android.widget.TextView[@text='" + text + "']")));
         return (MobileElement) dropdown;
     }
+//    public static MobileElement selectDropdownTextAbsolute(String text){
+//        WebElement dropdown = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(text)));
+//        return (MobileElement) dropdown;
+//    }
 
     public static MobileElement selectElement(String id) {
         WebElement chosenElement = wait.until(ExpectedConditions.presenceOfElementLocated(By.id(id)));
@@ -53,9 +51,81 @@ public class LokOS_Automation {
         WebElement chosenElement = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//android.widget.TextView[contains(@text,'" + myText + "')]")));
         return (MobileElement) chosenElement;
     }
+    public static  MobileElement selectRadio(String myText){
+        WebElement chosenElement = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//android.widget.RadioButton[contains(@text,'" + myText + "')]")));
+        return (MobileElement) chosenElement;
+    }
+
+    public static void main(String[] args) throws InterruptedException, IOException {
+        openLokOS();
+        navigateToSHG();
+        createSHG();
+    }
+
+    public static void openLokOS() {
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("deviceName", "Note 10 Pro");
+        capabilities.setCapability("189557ef", "RZ8M31X1Z9C");
+        capabilities.setCapability("platformName", "Android");
+        capabilities.setCapability("platformVersion", "12");
+        capabilities.setCapability("appPackage", "com.microware.cdfi.training");
+        capabilities.setCapability("appActivity", "com.microware.cdfi.activity.SplashScreenActivity");
+        capabilities.setCapability("noReset", "true");
+        capabilities.setCapability("newCommandTimeout", 10000);
+
+
+        URL url;
+        try {
+            url = new URL("http://127.0.0.1:4723/wd/hub");
+            driver = new AndroidDriver<MobileElement>(url, capabilities);
+        } catch (MalformedURLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            System.out.println("Error in URL");
+        }
+
+
+        System.out.println("App Opened");
+        wait = new WebDriverWait(driver, 300);
+        // enter password on login screen
+        WebElement enterPassword = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("com.microware.cdfi.training:id/otp_view")));
+        enterPassword.sendKeys("1234");
+
+
+        // wait for some time
+
+        System.out.println("Password entered");
+
+
+    }
+
+    public static void navigateToSHG() {
+        WebElement choose = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("com.microware.cdfi.training:id/tbl_shg")));
+        choose.click();
+        System.out.println("SHG selected");
+    }
+    public static void createSHG() throws IOException, InterruptedException{
+//        fillInfo();
+        createMembers();
+    }
+    public static void clickImage(){
+        //Camera Image Capture Working
+        // wait for camera to load
+        selectElement("com.android.camera:id/top_tip_layout");
+
+        // press camera button
+        driver.pressKey(new KeyEvent().withKey(AndroidKey.CAMERA));
+        selectElement("com.android.camera:id/done_button").click();
+        selectElement("com.microware.cdfi.training:id/crop_image_menu_crop").click();
+    }
 
 
     public static void fillInfo() throws IOException, InterruptedException {
+        // press the create SHG Button
+        WebElement create = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("com.microware.cdfi.training:id/IvAdd")));
+        create.click();
+
         System.out.println("Filling Info");
         // SHG Name
         MobileElement SHGName = scrollToId("com.microware.cdfi.training:id/et_groupname");
@@ -166,58 +236,86 @@ public class LokOS_Automation {
 //        // switch to page 5
 //        selectElement("com.microware.cdfi.training:id/IvKyc").click();
 
-
         // go to main page
         driver.manage().timeouts().implicitlyWait(4, TimeUnit.SECONDS);
         selectElement("com.microware.cdfi.training:id/ivBack").click();
 
     }
 
-    public static void navigateSHG() {
-        WebElement choose = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("com.microware.cdfi.training:id/tbl_shg")));
-        choose.click();
-        System.out.println("SHG selected");
+    public static void createMembers(){
+        selectElement("com.microware.cdfi.training:id/tv_count").click();
+        selectElement("com.microware.cdfi.training:id/tbl_add").click();
 
-        WebElement create = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("com.microware.cdfi.training:id/IvAdd")));
-        create.click();
+        // fill member details
+        selectElement("com.microware.cdfi.training:id/et_name").sendKeys("Test Member");
+
+        // selection the layout group and the get the radio child and select the No one
+        MobileElement parentRadio = scrollToId("com.microware.cdfi.training:id/rgisDobavailable");
+        parentRadio.findElements(By.className("android.widget.RadioButton")).get(1).click();
+
+        scrollToId("com.microware.cdfi.training:id/et_age").sendKeys("40");
+
+        // change the marital status
+        scrollToId("com.microware.cdfi.training:id/spin_martial").click();
+        selectDropdownText("Married").click();
+
+        scrollToId("com.microware.cdfi.training:id/et_mother_father").sendKeys("Test Spouse");
+
+        // social category
+        scrollToId("com.microware.cdfi.training:id/spin_socialCategory").click();
+        selectDropdownText("General").click();
+
+        // Religion
+        scrollToId("com.microware.cdfi.training:id/spin_religion").click();
+        selectDropdownText("Hindu").click();
+
+        // Highest Education
+        scrollToId("com.microware.cdfi.training:id/spin_education").click();
+        selectDropdownText("Diploma").click();
+
+        // No to insurance
+        scrollToId("com.microware.cdfi.training:id/rgInsurance");
+        MobileElement insuranceRadio = scrollToId("com.microware.cdfi.training:id/rgInsurance");
+        insuranceRadio.findElements(By.className("android.widget.RadioButton")).get(1).click();
+
+        // Primary Livelihood
+        scrollToId("com.microware.cdfi.training:id/spin_occupation").click();
+        selectDropdownText("Agriculture Activities").click();
+
+        // Secondary Livelihood
+        scrollToId("com.microware.cdfi.training:id/spin_secondaryoccu").click();
+        selectDropdownText("Horticulture Activities").click();
+
+        // Tertiary Livelihood
+        scrollToId("com.microware.cdfi.training:id/spin_tertiaryoccu").click();
+        selectDropdownText("Fishery Activities").click();
+
+        // Member Image
+        scrollToId("com.microware.cdfi.training:id/Imgmember").click();
+        clickImage();
+
+        // Aadhar Form
+        scrollToId("com.microware.cdfi.training:id/ImgmemberConsent").click();
+        clickImage();
+
+        scrollToId("com.microware.cdfi.training:id/btn_save").click();
+        selectElement("com.microware.cdfi.training:id/btn_ok").click();
+        selectElement("com.microware.cdfi.training:id/btn_ok").click();
+
+
+        // Add Phone Number Page
+        selectElement("com.microware.cdfi.training:id/addphone");
+        
+
+
+
+
+
     }
 
-    public static void openLokOS() {
-
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("deviceName", "Note 10 Pro");
-        capabilities.setCapability("189557ef", "RZ8M31X1Z9C");
-        capabilities.setCapability("platformName", "Android");
-        capabilities.setCapability("platformVersion", "12");
-        capabilities.setCapability("appPackage", "com.microware.cdfi.training");
-        capabilities.setCapability("appActivity", "com.microware.cdfi.activity.SplashScreenActivity");
-        capabilities.setCapability("noReset", "true");
-        capabilities.setCapability("newCommandTimeout", 10000);
 
 
-        URL url;
-        try {
-            url = new URL("http://127.0.0.1:4723/wd/hub");
-            driver = new AndroidDriver<MobileElement>(url, capabilities);
-        } catch (MalformedURLException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-            System.out.println("Error in URL");
-        }
 
 
-        System.out.println("App Opened");
-        wait = new WebDriverWait(driver, 300);
-        // enter password on login screen
-        WebElement enterPassword = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("com.microware.cdfi.training:id/otp_view")));
-        enterPassword.sendKeys("1234");
-
-
-        // wait for some time
-
-        System.out.println("Password entered");
-
-
-    }
 
 }
