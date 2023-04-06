5c53c1b0d8f933f734b692fa88a8d8b0f6e1b980
VIBHOR AGARWAL <vibhor20349@iiitd.ac.in>
2023-04-06 22:11:10 +0530

    Added Auto Appium start if Node.js installed and used better paths

---
+++ src/test/java/AppiumServer.java
@@ -0,0 +1,49 @@
+import org.openqa.selenium.remote.DesiredCapabilities;
+import io.appium.java_client.service.local.AppiumDriverLocalService;
+import io.appium.java_client.service.local.AppiumServiceBuilder;
+import io.appium.java_client.service.local.flags.GeneralServerFlag;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+
+public class AppiumServer {
+
+    private AppiumDriverLocalService service;
+
+    public void startServer() {
+        //Set Capabilities
+        DesiredCapabilities cap = new DesiredCapabilities();
+
+        //Build the Appium service
+        AppiumServiceBuilder builder = new AppiumServiceBuilder();
+        builder.withIPAddress("127.0.0.1");
+        builder.usingPort(4723);
+        builder.withCapabilities(cap);
+        builder.withArgument(GeneralServerFlag.SESSION_OVERRIDE);
+        builder.withArgument(GeneralServerFlag.LOG_LEVEL, "error");
+
+        //Start the server with the builder
+        service = AppiumDriverLocalService.buildService(builder);
+        service.start();
+    }
+
+    public void stopServer() {
+        service.stop();
+    }
+
+    public boolean checkIfServerIsRunnning(int port) {
+
+        boolean isServerRunning = false;
+        ServerSocket serverSocket;
+        try {
+            serverSocket = new ServerSocket(port);
+            serverSocket.close();
+        } catch (IOException e) {
+            //If control comes here, then it means that the port is in use
+            isServerRunning = true;
+        } finally {
+            serverSocket = null;
+        }
+        return isServerRunning;
+    }
+}
